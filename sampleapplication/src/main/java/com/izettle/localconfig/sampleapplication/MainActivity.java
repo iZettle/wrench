106342303eca3362e5738e793d410df3d78d29e6
Erik Eelde <erik.eelde@izettle.com>
2016-11-03 00:19:29 +0100

    remove exists from api

--- sampleapplication/src/main/java/com/izettle/localconfig/sampleapplication/MainActivity.java
+++ sampleapplication/src/main/java/com/izettle/localconfig/sampleapplication/MainActivity.java
@@ -25,37 +25,32 @@ public class MainActivity extends AppCompatActivity {
 
         LocalConfiguration localConfiguration = new LocalConfiguration(this);
 
-        if (!BuildConfig.DEBUG || localConfiguration.exists()) {
-            // text that defaults to something
-            log("WELCOME_TITLE:");
-            log(localConfiguration.getString("WELCOME_TITLE", "welcome!"));
+        // text that defaults to something
+        log("WELCOME_TITLE:");
+        log(localConfiguration.getString("WELCOME_TITLE", "welcome!"));
 
-            // welcome text that can be null
-            log("WELCOME_TEXT:");
-            log(localConfiguration.getString("WELCOME_TEXT", null));
+        // welcome text that can be null
+        log("WELCOME_TEXT:");
+        log(localConfiguration.getString("WELCOME_TEXT", null));
 
-            // check if strict mode is enabled, default to false/DISABLED
-            log("STRICT_MODE:");
-            log(localConfiguration.getBoolean("STRICT_MODE", false) ? "ENABLED" : "DISABLED");
+        // check if strict mode is enabled, default to false/DISABLED
+        log("STRICT_MODE:");
+        log(localConfiguration.getBoolean("STRICT_MODE", false) ? "ENABLED" : "DISABLED");
 
-            // get num columns to show, defaults to 2
-            log("NUM_COLUMNS:");
-            log(String.valueOf(localConfiguration.getInt("NUM_COLUMNS", 2)));
+        // get num columns to show, defaults to 2
+        log("NUM_COLUMNS:");
+        log(String.valueOf(localConfiguration.getInt("NUM_COLUMNS", 2)));
 
 
-            log("All keys and values:");
-            for (Map.Entry<String, ?> entry : localConfiguration.getAll().entrySet()) {
-                if (entry.getValue() != null) {
-                    log(entry.getKey() + ": (" + entry.getValue().getClass().getSimpleName() + ")" + entry.getValue());
-                } else {
-                    log(entry.getKey() + ": (?) NULL ");
-                }
+        log("All keys and values:");
+        for (Map.Entry<String, ?> entry : localConfiguration.getAll().entrySet()) {
+            if (entry.getValue() != null) {
+                log(entry.getKey() + ": (" + entry.getValue().getClass().getSimpleName() + ")" + entry.getValue());
+            } else {
+                log(entry.getKey() + ": (?) NULL ");
             }
-
-
-        } else {
-            log("Local configuration application not installed");
         }
+
 
     }
 }
