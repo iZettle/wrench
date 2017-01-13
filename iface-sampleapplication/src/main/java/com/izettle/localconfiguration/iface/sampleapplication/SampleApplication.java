082d60af61da9c0be8a8adcc09246619a2a481a7
Erik Eelde <erik.eelde@gmail.com>
2017-01-14 00:33:26 +0100

    remove interfacelibrary

--- iface-sampleapplication/src/main/java/com/izettle/localconfiguration/iface/sampleapplication/SampleApplication.java
+++
@@ -1,6 +0,0 @@
-package com.izettle.localconfiguration.iface.sampleapplication;
-
-import android.support.multidex.MultiDexApplication;
-
-public class SampleApplication extends MultiDexApplication {
-}
