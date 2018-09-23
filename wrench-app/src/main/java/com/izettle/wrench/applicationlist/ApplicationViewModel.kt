013186f63c3282eb65620e3edf19df0adfa232b9
Erik Eelde <erik.eelde@izettle.com>
2018-09-23 22:36:48 +0200

    applicationlist -> kotlin

---
+++ wrench-app/src/main/java/com/izettle/wrench/applicationlist/ApplicationViewModel.kt
@@ -0,0 +1,45 @@
+package com.izettle.wrench.applicationlist
+
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.izettle.wrench.database.WrenchApplication
+import com.izettle.wrench.database.WrenchApplicationDao
+import javax.inject.Inject
+
+class ApplicationViewModel @Inject
+internal constructor(applicationDao: WrenchApplicationDao) : ViewModel() {
+
+    private val mediatedApplications: MediatorLiveData<PagedList<WrenchApplication>>
+
+    private val listEmpty: MutableLiveData<Boolean> = MutableLiveData()
+
+    internal val applications: LiveData<PagedList<WrenchApplication>>
+        get() = mediatedApplications
+
+    internal val isListEmpty: LiveData<Boolean>
+        get() = listEmpty
+
+    init {
+
+        listEmpty.value = true
+
+        val applications = LivePagedListBuilder(applicationDao.getApplications(),
+                PagedList.Config.Builder()
+                        .setEnablePlaceholders(true)
+                        .setPageSize(10)
+                        .setPrefetchDistance(10)
+                        .build()).build()
+
+        mediatedApplications = MediatorLiveData()
+
+        mediatedApplications.addSource(applications) { wrenchApplications ->
+            listEmpty.value = wrenchApplications == null || wrenchApplications.size == 0
+            mediatedApplications.setValue(wrenchApplications)
+        }
+    }
+}
