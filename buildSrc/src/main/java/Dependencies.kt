164d60de11a8f59358813e637ef30a8fca6de08d
Erik Eelde <erik.eelde@izettle.com>
2018-10-15 21:50:40 +0200

    Bump app version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,8 +5,8 @@ object Versions {
     const val compileSdk = 28
     const val minSdk = 14
 
-    const val appVersionCode = 11
-    const val appVersionName = "1.0.10"
+    const val appVersionCode = 12
+    const val appVersionName = "1.0.11"
 
     const val dagger = "2.16"
 
